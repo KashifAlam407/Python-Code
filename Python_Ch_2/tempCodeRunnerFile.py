a62a3c673cefaca297e9cf9526d65a6bf551cc02
Md Kashif Alam <kingkhankhan0407@gmail.com>
2025-04-24 15:18:21 +0530

    PW IOI
    Python Class Work

--- Python_Ch_2/tempCodeRunnerFile.py
+++ Python_Ch_2/tempCodeRunnerFile.py
@@ -1,1 +1,22 @@
-print('pratik','geeksforgeeks', sep='@')
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+h = int(input("Enter the value of h: "))
+
+Area = 1/2*(a+b)*h
+print("Area of the triangle is: ", Area)
+
+Area = 3**0.5*a**2/4
+print("Area of the triangle is: ", Area)
+
+exp = (a+b)**0.5*1/2*(a**2 + b**2)
+print(exp)
+
+s = (a+b+c)/2
+print(s)
+
+A = (s*(s-a)*(s-b)*(s-c))**0.5
+print(A)
+
+T = 2*22/7*(a/b)**0.5
+print(T)
