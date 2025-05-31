486256083c284e338a2b9b06d9cae4a8705c2969
Md Kashif Alam <kingkhankhan0407@gmail.com>
2025-05-31 16:35:46 +0530

    Basic Changes

--- PW IOI/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-sum = 0
-for i in range(2, 11):
-    sum = sum + i
-    
-print("Sum of numbers from 2 to 10 is:", sum)
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-gand = int(input())
-if gand>=34:
-    print("GAND OF MANISH IS VERY VERY LARGE")
-else:
-    print("MANISH KA BHOSDA")
-print("Manish bosdeke, gandu, laudu, ladchat,")
